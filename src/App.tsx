ad48808c9a1c55a117df3054806c112841fc1daa
David M. Chen <chen7david@me.com>
2023-02-06 05:49:52 -0500

    Revert "add a basic layout"

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,14 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
-import { MenuUnfoldOutlined, CloseOutlined } from "@ant-design/icons";
-import { Layout, Menu } from 'antd'
-const { Content, Header, Sider } = Layout
+
 function App() {
   const [count, setCount] = useState(0)
-  const [collapsed, setCollapsed] = useState(false)
 
   return (
-    <Layout style={{ height: '100vh'}}>
-      <Header style={{ background: "white" }}>
-      <div className="logo" />
-          {collapsed ? (
-            <MenuUnfoldOutlined onClick={() => setCollapsed(!collapsed)} />
-          ) : (
-            <CloseOutlined onClick={() => setCollapsed(!collapsed)} />
-          )}
-      </Header>
-      <Layout>
-        <Sider
-          trigger={null}
-          collapsible
-          collapsed={collapsed}
-        >
-          <Menu></Menu>
-        </Sider>
-        <Content>
-
-        </Content>
-      </Layout>
-    </Layout>
+    <div className="App">
+      hello world!
+    </div>
   )
 }
 
