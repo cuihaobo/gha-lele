094fb8eeef8d619a0acf7e39247654587e65ae96
cuihoabo <Lelecui123@163.com>
2023-03-06 19:40:07 +0800

    testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,66 @@
-import { FrownOutlined, MehOutlined, SmileOutlined } from "@ant-design/icons";
+import { AppstoreOutlined, FrownOutlined, MailOutlined, MehOutlined, SettingOutlined, SmileOutlined } from "@ant-design/icons";
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
+import type { MenuProps } from 'antd';
+const items: MenuProps["items"] = [
+  {
+    label: "Navigation One",
+    key: "mail",
+    icon: <MailOutlined />,
+  },
+  {
+    label: "Navigation Two",
+    key: "app",
+    icon: <AppstoreOutlined />,
+    disabled: true,
+  },
+  {
+    label: "Navigation Three - Submenu",
+    key: "SubMenu",
+    icon: <SettingOutlined />,
+    children: [
+      {
+        type: "group",
+        label: "Item 1",
+        children: [
+          {
+            label: "Option 1",
+            key: "setting:1",
+          },
+          {
+            label: "Option 2",
+            key: "setting:2",
+          },
+        ],
+      },
+      {
+        type: "group",
+        label: "Item 2",
+        children: [
+          {
+            label: "Option 3",
+            key: "setting:3",
+          },
+          {
+            label: "Option 4",
+            key: "setting:4",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    label: (
+      <a href="https://ant.design" target="_blank" rel="noopener noreferrer">
+        Navigation Four - Link
+      </a>
+    ),
+    key: "alipay",
+  },
+];
+
+
 import { MenuUnfoldOutlined, CloseOutlined } from "@ant-design/icons";
 import {
   Layout,
@@ -73,9 +132,17 @@ function App() {
                 </Card>
                 <Card title="Lakers" size="small">
                   <p>Let's join LA Lakers!!!</p>
+                  <img
+                      alt="example"
+                      src="https://pics0.baidu.com/feed/314e251f95cad1c81c150613aa253d0fc93d517e.jpeg"
+                    />
                   <QRCode value="https://china.nba.com/lakers" />
                 </Card>
-                
+                <Menu 
+                    onClick={() => console.log("dummy")}
+                    mode="horizontal"
+                    items={(items)}
+                />
                 <Space direction="vertical">
                   <DatePicker onChange={onchange} />
                   <DatePicker onChange={onchange} picker="week" />
